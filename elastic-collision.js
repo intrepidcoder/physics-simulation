a35ae616dffad1fc4d2f9de41eee0cba0317e04a
intrepidcoder <intrepidcoder@users.noreply.github.com>
2016-02-13 17:31:58 -0500

    Add nonfunctional button and input controls
    Add number controls to specify the mass and velocity of red and blue
    balls and a start/stop button. Wrap code in IIFE to enforce strict mode.

--- elastic-collision.js
+++ elastic-collision.js
@@ -1,87 +1,92 @@
-
-// Declare global variables.
-var scene, camera, renderer, materialBlue, materialRed, controls;
-var sphere1, sphere2;
-
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-renderer = new THREE.WebGLRenderer();
-
-// Set coordinates for camera.
-camera.position.x = 0;
-camera.position.y = 300;
-camera.position.z = 300;
-
-// Add the renderer to the page.
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Set background color to white.
-renderer.setClearColor(0xffffff, 1);
-
-document.body.appendChild(renderer.domElement);
-
-// Create a basic blue material for the first sphere.
-materialBlue = new THREE.MeshBasicMaterial({'color': 0x2196F3});
-materialRed = new THREE.MeshBasicMaterial({'color': 0xf44336});
-
-// Add orbit controls to enable moving around.
-controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-// Create two sphere objects.
-// SphereGeometry(radius, widthSegments, heightSegments);
-sphere1 = new THREE.Mesh(new THREE.SphereGeometry(50, 30, 30), materialBlue);
-sphere2 = new THREE.Mesh(new THREE.SphereGeometry(50, 30, 30), materialRed);
-
-// Set positions for spheres.
-sphere1.position.set(200, 0, 0);
-sphere2.position.set(-200, 0, 0);
-
-scene.add(sphere1);
-scene.add(sphere2);
-
-// The speed is measured in units/second.
-sphere1.velocity = 50;
-sphere2.velocity = 50;
-
-sphere1.radius = 50;
-sphere2.radius = 50;
-
-var previousTime = 0;
-
-function render(time) {
+// Wrap entire script in immediately-invoked function expression (IIFE) to enforce strict mode.
+((function(){
     'use strict';
-    
-    // time is a decimal number representing the time in milliseconds.
-    // Calculate the change in time in seconds since the last frame.
-    var deltaTime = (time - previousTime) * 0.001;
-    previousTime = time;
 
-    // Check for a collision and reverse direction.
-    if (Math.abs(sphere1.position.x - sphere2.position.x + deltaTime * (sphere2.velocity - sphere1.velocity)) <= sphere1.radius + sphere2.radius) {
-        // Calclate new velocities.
-        sphere1.velocity *= -1;
-        sphere2.velocity *= -1;
+    // Declare variables.
+    var scene, camera, renderer, materialBlue, materialRed, controls;
+    var sphere1, sphere2;
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    renderer = new THREE.WebGLRenderer();
+
+    // Set coordinates for camera.
+    camera.position.x = 0;
+    camera.position.y = 300;
+    camera.position.z = 300;
+
+    // Add the renderer to the page.
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Set background color to white.
+    renderer.setClearColor(0xffffff, 1);
+
+    document.body.appendChild(renderer.domElement);
+
+    // Create a basic blue material for the first sphere.
+    materialBlue = new THREE.MeshBasicMaterial({'color': 0x2196F3});
+    materialRed = new THREE.MeshBasicMaterial({'color': 0xf44336});
+
+    // Add orbit controls to enable moving around.
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+    // Create two sphere objects.
+    // SphereGeometry(radius, widthSegments, heightSegments);
+    sphere1 = new THREE.Mesh(new THREE.SphereGeometry(50, 30, 30), materialBlue);
+    sphere2 = new THREE.Mesh(new THREE.SphereGeometry(50, 30, 30), materialRed);
+
+    // Set positions for spheres.
+    sphere1.position.set(200, 0, 0);
+    sphere2.position.set(-200, 0, 0);
+
+    scene.add(sphere1);
+    scene.add(sphere2);
+
+    // The speed is measured in units/second.
+    sphere1.velocity = 50;
+    sphere2.velocity = 50;
+
+    sphere1.radius = 50;
+    sphere2.radius = 50;
+
+    var previousTime = 0;
+
+    // Draw everything.
+    function render(time) {
+
+        // time is a decimal number representing the time in milliseconds.
+        // Calculate the change in time in seconds since the last frame.
+        var deltaTime = (time - previousTime) * 0.001;
+        previousTime = time;
+
+        // Check for a collision and reverse direction.
+        if (Math.abs(sphere1.position.x - sphere2.position.x + deltaTime * (sphere2.velocity - sphere1.velocity)) <= sphere1.radius + sphere2.radius) {
+            // Calclate new velocities.
+            sphere1.velocity *= -1;
+            sphere2.velocity *= -1;
+        }
+
+        // Update positions of spheres.
+        sphere1.position.x -= deltaTime * sphere1.velocity;
+        sphere2.position.x += deltaTime * sphere2.velocity;
+
+        // Update the scene.
+        renderer.render(scene, camera);
+
+        // Add animation frame to call render().
+        requestAnimationFrame(render);
     }
 
-    // Update positions of spheres.
-    sphere1.position.x -= deltaTime * sphere1.velocity;
-    sphere2.position.x += deltaTime * sphere2.velocity;
+    requestAnimationFrame(render); // Start animation.
 
-    // Update the scene.
-    renderer.render(scene, camera);
+    // Create a function that is called whenever the window is resized.
+    window.addEventListener('resize', function() {
 
-    // Add animation frame to call render().
-    requestAnimationFrame(render);
-}
+        // Resize the canvas.
+        var width = window.innerWidth, height = window.innerHeight;
+        renderer.setSize(width, height);
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    });
 
-requestAnimationFrame(render); // Start animation.
-
-// Create a function that is called whenever the window is resized.
-window.addEventListener('resize', function() {
-    'use strict';
-
-    var width = window.innerWidth, height = window.innerHeight;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-});
+})()); // End IIFE.
