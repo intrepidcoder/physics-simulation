7b8ee1e4847ff8a29b8bb70b0eee97101f6a2ebd
intrepidcoder <intrepidcoder@users.noreply.github.com>
2016-02-14 12:09:40 -0500

    Improve how buttons work. Add more comments
    Fix some user-experience issues. Update mass and velocity when the start
    button is pressed after the spheres have been reset. Add limits for mass
    and velocity. Add/improve comments.

--- elastic-collision.js
+++ elastic-collision.js
@@ -1,65 +1,53 @@
-// Wrap entire script in immediately-invoked function expression (IIFE) to enforce strict mode.
-((function(){
+// This function is called immediately on execution.
+((function main(){
+    // Enforce stricter syntax.
     'use strict';
 
     // Declare variables.
-    var scene, camera, renderer, materialBlue, materialRed, controls;
-    var sphere1 = {}, sphere2 = {};
+    var scene,
+    camera,
+    renderer,
+    materialBlue,
+    materialRed,
+    controls,
+    sphere1 = {}, // Create objects to store the various properties of the spheres.
+    sphere2 = {},
+    previousTime = 0,
+    running = false,
+    reset = true;
 
+    // Create objects for 3D environment.
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 2000);
     renderer = new THREE.WebGLRenderer();
 
-    // Set coordinates for camera.
+    // Set possition of camera.
     camera.position.x = 0;
     camera.position.y = 300;
     camera.position.z = 300;
 
-    // Add the renderer to the page.
+    // Set the size of the renderer.
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     // Set background color to white.
     renderer.setClearColor(0xffffff, 1);
 
+    // Add the canvas to the page.
     document.body.appendChild(renderer.domElement);
 
-    // Create a basic blue material for the first sphere.
+    // Create basic blue and red materials for the spheres.
     materialBlue = new THREE.MeshBasicMaterial({'color': 0x2196F3});
     materialRed = new THREE.MeshBasicMaterial({'color': 0xf44336});
 
-    // Add orbit controls to enable moving around.
+    // Add orbit controls to allow looking around.
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    // Create two sphere objects.
-    // SphereGeometry(radius, widthSegments, heightSegments);
-    sphere1.mesh = new THREE.Mesh(new THREE.SphereGeometry(50, 30, 30), materialRed);
-    sphere2.mesh = new THREE.Mesh(new THREE.SphereGeometry(50, 30, 30), materialBlue);
-
-    // Set positions for spheres.
-    sphere1.mesh.position.set(-200, 0, 0);
-    sphere2.mesh.position.set(200, 0, 0);
-
-    scene.add(sphere1.mesh);
-    scene.add(sphere2.mesh);
-
-    // The speed is measured in units/second.
-    sphere1.velocity = 50;
-    sphere2.velocity = 50;
-
-    sphere1.mass = 1000;
-    sphere2.mass = 1000;
-
-    // Assuming constant density, the radius is proportional to the cube root of mass.
-    sphere1.radius = 5 * Math.pow(sphere1.mass, 1/3);
-    sphere2.radius = 5 * Math.pow(sphere2.mass, 1/3);
-
-    var previousTime = 0, running = false;
-
-    // Draw everything.
+    // Update the environment to create animation. Loops continuously.
     function render(time) {
-        var deltaTime, distance;
+        var deltaTime,
+        distance;
 
-        // time is a decimal number representing the time in milliseconds.
+        // The variable time is a floating point number that contains the time in milliseconds.
         // Calculate the change in time in seconds since the last frame.
         deltaTime = (time - previousTime) * 0.001;
         previousTime = time;
@@ -67,8 +55,8 @@
         // Move spheres.
         if (running) {
             // Check for a collision and reverse direction.
-            distance = Math.abs(sphere1.mesh.position.x - sphere2.mesh.position.x + deltaTime * (sphere2.velocity - sphere1.velocity));
-            if (distance <= sphere1.radius + sphere2.radius && sphere1.velocity > 0) {
+            distance = Math.abs(sphere1.mesh.position.x - sphere2.mesh.position.x) - 0*deltaTime * (sphere2.velocity + sphere1.velocity);
+            if (distance <= sphere1.radius + sphere2.radius && (sphere1.velocity > 0 || sphere2.velocity > 0)) {
                 // Calclate new velocities.
                 sphere1.velocity *= -1;
                 sphere2.velocity *= -1;
@@ -82,46 +70,86 @@
         // Update the scene.
         renderer.render(scene, camera);
 
-        // Add animation frame to call render().
+        // Call render function again as soon as possible.
         requestAnimationFrame(render);
     }
 
-    window.addEventListener('load', function () {
+    // Reset the spheres to their initial positions.
+    function updateSpheres() {
+        // Remove the old spheres from the environment if necessary.
+        scene.remove(sphere1.mesh);
+        scene.remove(sphere2.mesh);
+
+        // Read the mass and velocity from the input controls.
+        sphere1.mass = parseInt(document.getElementById('mass1').value, 10);
+        sphere2.mass = parseInt(document.getElementById('mass2').value, 10);
+        sphere1.velocity = parseInt(document.getElementById('velocity1').value, 10);
+        sphere2.velocity = parseInt(document.getElementById('velocity2').value, 10);
+
+        // Limit range of mass and velocity.
+        sphere1.mass = Math.min(Math.max(sphere1.mass, 1), 10000);
+        sphere2.mass = Math.min(Math.max(sphere2.mass, 1), 10000);
+        sphere1.velocity = Math.min(Math.max(sphere1.velocity, 0), 1000);
+        sphere2.velocity = Math.min(Math.max(sphere2.velocity, 0), 1000);
+
+        // Assuming constant density, the radius of a sphere is proportional to the cube root of the mass.
+        sphere1.radius = 5 * Math.pow(sphere1.mass, 1/3);
+        sphere2.radius = 5 * Math.pow(sphere2.mass, 1/3);
+
+        // Create 3D mesh objects to display the spheres in the environment.
+        // The arguments for SphereGeometry are: radius, widthSegments, heightSegments.
+        sphere1.mesh = new THREE.Mesh(new THREE.SphereGeometry(sphere1.radius, 30, 30), materialRed);
+        sphere2.mesh = new THREE.Mesh(new THREE.SphereGeometry(sphere2.radius, 30, 30), materialBlue);
+
+        // Set the spheres' positions.
+        sphere1.mesh.position.set(-200, 0, 0);
+        sphere2.mesh.position.set(200, 0, 0);
+
+        // Add the new spheres to the environment.
+        scene.add(sphere1.mesh);
+        scene.add(sphere2.mesh);
+    }
+
+    // Create a function that is called once the window is fully loaded.
+    window.addEventListener('load', function() {
+        updateSpheres();
+
+        // Create a function that is called when the start button is clicked.
         document.getElementById('start-button').addEventListener('click', function() {
             if (running) {
+                // Pause the animation
                 running = false;
                 this.value = 'Start';
+                document.getElementById('reset-button').disabled = false;
 
             } else {
-                running = true;
-                this.value = 'Stop';
+                // If the spheres have been reset, update them in case the controls have been changed.
+                if (reset) {
+                    updateSpheres();
+                }
 
+                // Start the animation.
+                running = true;
+                reset = false;
+                this.value = 'Stop';
+                document.getElementById('reset-button').disabled = true;
             }
         }, false);
 
+        // Create a function that is called when the reset button is clicked.
         document.getElementById('reset-button').addEventListener('click', function() {
-            scene.remove(sphere1.mesh);
-            scene.remove(sphere2.mesh);
-
-            sphere1.mass = parseInt(document.getElementById('mass1').value, 10);
-            sphere2.mass = parseInt(document.getElementById('mass2').value, 10);
-            sphere1.velocity = parseInt(document.getElementById('velocity1').value, 10);
-            sphere2.velocity = parseInt(document.getElementById('velocity2').value, 10);
-            sphere1.radius = 5 * Math.pow(sphere1.mass, 1/3);
-            sphere2.radius = 5 * Math.pow(sphere2.mass, 1/3);
-
-            // Create two sphere objects.
-            sphere1.mesh = new THREE.Mesh(new THREE.SphereGeometry(sphere1.radius, 30, 30), materialRed);
-            sphere2.mesh = new THREE.Mesh(new THREE.SphereGeometry(sphere2.radius, 30, 30), materialBlue);
-            sphere1.mesh.position.set(-200, 0, 0);
-            sphere2.mesh.position.set(200, 0, 0);
-            scene.add(sphere1.mesh);
-            scene.add(sphere2.mesh);
+            if (!running) {
+                // Reset the spheres.
+                updateSpheres();
+                reset = true;
+            }
         }, false);
-    }, false);
 
+        document.getElementById('controls').addEventListener('keydown', function(event) {
+            event.stopPropagation();
+        }, false);
+    }, false); // End window load event.
 
-    requestAnimationFrame(render); // Start animation.
 
     // Create a function that is called whenever the window is resized.
     window.addEventListener('resize', function() {
@@ -133,4 +161,7 @@
         camera.updateProjectionMatrix();
     });
 
-})()); // End IIFE.
+    // Start animation.
+    requestAnimationFrame(render);
+
+})()); // End main.
