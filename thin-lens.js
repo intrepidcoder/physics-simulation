02b46109a4a7966c91f67e0de727380cd42d8b53
intrepidcoder <intrepidcoder@users.noreply.github.com>
2016-03-06 10:39:08 -0500

    Draw converging lens, focal points, and optical axis
    Draw a thin converging lens at a fixed height in the center of the
    canvas, treating the focal length as a parameter. Also draw the focal
    and center points of the lens as circles and the optical axis as a
    dashed line.

--- thin-lens.js
+++ thin-lens.js
@@ -6,12 +6,50 @@
         context,
         self = this,
         width,
-        height;
+        height,
+        focalLength = 300;
 
         var drawStatic = function() {
-            context.fillStyle = '#000';
-            context.fillRect(10, 10, width - 20, 50);
-            context.fillRect(Math.floor(width / 2) - 5, Math.floor(height / 2) - 5, 10, 10);
+            var centerX = Math.floor(width / 2),
+            centerY = Math.floor(height / 2),
+            angle = Math.asin(100 / focalLength);
+
+            // Draw dashed center line
+            context.setLineDash([20, 20]);
+            context.lineDashOffset = 10;
+            // Draw in two segments so dashes are centered
+            context.beginPath();
+            context.moveTo(centerX, centerY);
+            context.lineTo(0, centerY);
+            context.stroke();
+            context.beginPath();
+            context.moveTo(centerX, centerY);
+            context.lineTo(width, centerY);
+            context.stroke();
+            context.setLineDash([1, 0]);
+
+
+            // Draw lens
+            context.lineWidth = 2;
+            context.strokeStyle = '#666';
+            context.beginPath();
+            context.arc(centerX - 2 * focalLength + focalLength * 2 * (1 - Math.cos(angle)), centerY, focalLength * 2, -angle, angle, false);
+            context.stroke();
+            context.beginPath();
+            context.arc(centerX + 2 * focalLength - focalLength * 2 * (1 - Math.cos(angle)), centerY, focalLength * 2, Math.PI - angle, Math.PI + angle, false);
+            context.stroke();
+            context.strokeStyle = '#000';
+
+            // Draw focal points
+            context.beginPath();
+            context.arc(centerX, centerY, 5, 0, 2 * Math.PI, false);
+            context.fill();
+            context.beginPath();
+            context.arc(centerX - focalLength, centerY, 5, 0, 2 * Math.PI, false);
+            context.fill();
+            context.beginPath();
+            context.arc(centerX + focalLength, centerY, 5, 0, 2 * Math.PI, false);
+            context.fill();
         };
 
         this.draw = function() {
